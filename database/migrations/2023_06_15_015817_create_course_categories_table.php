54e07ad5e442898300b29399d03604b62c65f374
yuya304 <yuyaaoki34@gmail.com>
2023-06-15 13:49:21 +0900

    追加：マイグレーションファイル, マイグレーション実行前

---
+++ database/migrations/2023_06_15_015817_create_course_categories_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('course_categories', function (Blueprint $table) {
+            $table->integer('course_id');
+            $table->integer('subject_id');
+            $table->integer('attribute_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('course_categories');
+    }
+};
