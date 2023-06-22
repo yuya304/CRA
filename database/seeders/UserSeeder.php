3ac8ef1d59993fa71060b8e8c7ca7439cb01db80
yuya304 <yuyaaoki34@gmail.com>
2023-06-22 11:03:12 +0900

    追加：index.blade.php & style.css

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use DateTime;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'course_id' => '5',
+            'student_number' => 20216002,
+            'name' => '青木悠弥',
+            'email' => 'yuyaaoki34@gmail.com',
+            'password' => Hash::make('password'),
+            'grade' => 3,
+            'admission' => 2021,
+        ]);
+    }
+}
